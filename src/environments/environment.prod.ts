8c3d3090c47ce9438a18fec6eb9cef4f0ca3639c
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-05-19 19:18:10 +0300

    Firebase connector implemented

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB7FLwm1l4ECBjrpEsJQjLV_AHz3yituEg",
+    authDomain: "angular-shop-8c00a.firebaseapp.com",
+    databaseURL: "https://angular-shop-8c00a.firebaseio.com",
+    projectId: "angular-shop-8c00a",
+    storageBucket: "angular-shop-8c00a.appspot.com",
+    messagingSenderId: "341268045496",
+    appId: "1:341268045496:web:a723b2381d6383bd239e4d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB7FLwm1l4ECBjrpEsJQjLV_AHz3yituEg",
+    authDomain: "angular-shop-8c00a.firebaseapp.com",
+    databaseURL: "https://angular-shop-8c00a.firebaseio.com",
+    projectId: "angular-shop-8c00a",
+    storageBucket: "angular-shop-8c00a.appspot.com",
+    messagingSenderId: "341268045496",
+    appId: "1:341268045496:web:a723b2381d6383bd239e4d"
+  }
 };
 
 /*
