4fe4f6ebd0c25725066522599ea7516a5ff52780
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-05-22 22:31:50 +0300

    Adding blocking router

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { map } from 'rxjs/operators';
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate() {
+    this.auth.user$.subscribe(user => {
+      if(user) {
+        return true;
+      }
+      else {
+        this.router.navigate(['/login']);
+        return false;
+      }
+    });
+  }
 }
