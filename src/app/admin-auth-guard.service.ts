e0b5741ce00610b5b13616e086086304b54209bc
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-05-23 23:01:02 +0300

    Adding User interface

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
+import { UserService } from './user.service';
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthGuardService implements CanActivate{
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private userService: UserService) { }
 
-  canActivate() {
-
+  canActivate(): Observable<boolean> {
+    this.auth.user$.subscribe(user => {
+      return this.userService.get(user);
+    });
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import * as firebase from 'firebase';
+import { AppUser } from './models/app-user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class UserService {
       email: user.email
     });
   }
-  get(uid: string) {
+  get(uid: string): AngularFireObject<AppUser> {
     return this.db.object('/users/'+uid);
   }
 }
