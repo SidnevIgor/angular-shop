5948ed1c0f5864bcc7c887bc9e3b542044bba8c4
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-05-23 18:46:09 +0300

    Adding redirect to the login component

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 import { map, take } from 'rxjs/operators';
 import { Observable, of } from "rxjs";
@@ -11,13 +11,13 @@ export class AuthGuardService implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate(): boolean {
+  canActivate(route,state: RouterStateSnapshot): boolean {
   this.auth.user$.subscribe(user => {
       if(user) {
         return true;
       }
       else {
-        this.router.navigate(['/login']);
+        this.router.navigate(['/login'],{ queryParams: {returnUrl: state.url}});
         return false;
       }
     });
