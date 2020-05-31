ff149eba5d8d5666c6f61f1cd73b72eaf455df29
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-05-31 17:26:38 +0300

    URL validation completed

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -21,4 +21,8 @@ export class ProductFormComponent implements OnInit {
   isNumber(num) {
     return num.value>0?true:false;
   }
+  isUrl(url) {
+    let re = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g;
+    return re.test(url.value)?true:false;
+  }
 }
