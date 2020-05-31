be7778f5e14b4b09ef5da2d0fd88481037f4d6d1
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-05-31 20:54:05 +0300

    Getting the list of all products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../product.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
-
-  constructor() { }
+  products;
+  constructor(private productServ: ProductService) {
+    this.productServ.getAll().valueChanges().subscribe(arr => {
+      this.products = arr;
+    })
+  }
 
   ngOnInit(): void {
   }
