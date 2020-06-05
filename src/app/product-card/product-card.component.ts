e34a8b06250a2ba9392c08c0f622729dfda9175d
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-06-05 19:05:50 +0300

    rf product card component

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
-  selector: 'app-product-card',
+  selector: 'product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent {
+  @Input('product') product: Product;
+  @Input('show-actions') showActions: boolean = true;
+  constructor() {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
 }
