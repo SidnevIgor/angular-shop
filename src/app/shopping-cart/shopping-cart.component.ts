18c00d17399dfdbecd8c0805d624c4aa350d9cd5
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-06-15 19:07:48 +0300

    Showing shopping cart contents

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -13,25 +13,28 @@ export class ShoppingCartComponent implements OnInit {
   cart$:any;
   numberOfItems: number;
   subscription: any;
+  productIds: any[];
   constructor(private shoppingCartServ: ShoppingCartService) { }
 
   async ngOnInit() {
     this.subscription = (await (await this.shoppingCartServ.getCart()).valueChanges()).subscribe(cart => {
       this.cart$ = cart;
-    });
-    let items: ShoppingCartItem[];
-    this.cart$.valueChanges().subscribe(cart => {
+      //console.log(this.cart$);
+      let items: ShoppingCartItem[];
       items = (<ShoppingCart>cart).items;
+      console.log(items);
       this.numberOfItems = 0;
       for(let productId in items) {
         this.numberOfItems+=items[productId].quantity;
       }
-    })
+      this.getProductIds();
+    });
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
   getProductIds() {
-    return Object.keys(this.cart$.items);
+    console.log(Object.keys(this.cart$.items));
+    this.productIds = Object.keys(this.cart$.items);
   }
 }
