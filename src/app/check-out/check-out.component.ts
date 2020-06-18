cdfde51af0e0d1b7368b72d1c78339d05f21432c
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-06-18 19:49:19 +0300

    Getting shopping cart

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent {
+export class CheckOutComponent implements OnInit, OnDestroy {
   shipping = {
     name: '',
     addressLine1: '',
     addressLine2: '',
     city: ''
   };
+  cart: ShoppingCart;
+  subscription: Subscription;
 
+  constructor(private shoppingCartServ: ShoppingCartService) {
+
+  }
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartServ.getCart();
+    this.subscription = cart$.valueChanges().subscribe(cart => {
+      this.cart = cart;
+    })
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
   placeOrder() {
-    console.log(this.shipping);
+    let order = {
+      datePlaced: new Date().getTime,
+      shipping: this.shipping,
+
+    }
   }
 }
