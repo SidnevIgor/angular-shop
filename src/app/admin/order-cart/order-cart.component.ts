45e30020a9bcf817db64ab74acf8d9bac271687b
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-06-29 17:48:30 +0300

    Deleting footer component

--- src/app/admin/order-cart/order-cart.component.ts
+++ src/app/admin/order-cart/order-cart.component.ts
@@ -5,6 +5,7 @@ import { OrderService } from '../../services/order.service';
 import { Order } from '../../models/order';
 import { Shipping } from '../../models/shipping';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'order-cart',
@@ -22,34 +23,40 @@ export class OrderCartComponent implements OnInit {
     addressLine1: '',
     addressLine2: ''
   };
-  totalPrice: number = 0;
+  totalPrice: number;
 
   constructor(private router:Router,
               private route: ActivatedRoute,
-              private ordServ: OrderService, private shoppingCartServ: ShoppingCartService) {
+              private ordServ: OrderService,
+              private shoppingCartServ: ShoppingCartService,
+              private db: AngularFireDatabase) {
     this.orderId = this.route.snapshot.paramMap.get('id');
     this.ordServ.getOrder(this.orderId).valueChanges().subscribe((ord) => {
       this.items = ord[1];
       this.shipping = ord[2];
+      this.totalPrice = 0;
       for(let i=0; i< this.items.length; i++) {
         if(this.items[i].quantity>0) {
+          console.log('CHECK', this.items[i]);
           this.totalPrice += this.items[i].quantity*this.items[i].product.price;
         }
       }
-      console.log('Items: ', this.items);
-      console.log('Shipping details: ', this.shipping);
     })
   }
   ngOnInit(): void {
 
   }
   save(form) {
-
+    console.log(this.items);
+    this.db.object('/orders/'+this.orderId+'/shipping').update(this.shipping);
+    this.db.object('/orders/'+this.orderId+'/items').update(this.items);
   }
   removeFromCart(item) {
     item.quantity--;
+    item.totalPrice = item.quantity*item.product.price;
   }
   addToCart(item) {
     item.quantity++;
+    item.totalPrice = item.quantity*item.product.price;
   }
 }
